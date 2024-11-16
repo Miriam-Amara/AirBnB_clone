730b336e6c1a8e859560c3584030253e2def105e
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-11-16 18:00:47 +0100

    add do_update method and refactor validate_command method

--- console.py
+++ console.py
@@ -12,13 +12,12 @@ from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-    
-    """
+    """ """
+
     prompt = "(hbnb) "
 
     def emptyline(self):
-        """ 
+        """
         Overides emptyline method to do nothing when a user press enter.
         """
         pass
@@ -26,32 +25,49 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, line):
         """Exits the program"""
         return True
 
-    
     def validate_command(self, line, command):
         """
         Displays customized error messages when commands are invalid
         """
-        args = line.split()
-        if command == "create":
-            if not args:
-                return "** class name missing **"
-            elif args[0] != "BaseModel":
-                return "** class doesn't exist **"
-                
-        elif command in ["show", "destroy", "all"]:   
-            if not args:
-                return "** class name missing **"
-            elif args[0] != "BaseModel":
-                return "** class doesn't exist **"
-            elif len(args) == 1:
-                return "** instance id missing **"
+        command_args = line.split()
+        msg0 = "** class name missing **"
+        msg1 = "** class doesn't exist **"
+        msg2 = "** instance id missing **"
+        msg3 = "** attribute name missing **"
+        msg4 = "** value missing **"
+        error_messages = {
+            "create": [msg0, msg1],
+            "show": [msg0, msg1, msg2],
+            "destroy": [msg0, msg1, msg2],
+            "update": [msg0, msg1, msg2, msg3, msg4],
+        }
+        if not command_args:
+            return error_messages[command][0]  # class name missing
+
+        if command_args[0] != "BaseModel" and command != "create":
+            return error_messages[command][1]  # class doesn't exist
+
+        if len(command_args) < 2 and command in ["show", "destroy", "update"]:
+            return error_messages[command][2]  # instance id missing
+
+        if command == "create" and (
+            len(command_args) > 1 or command_args[0] != "BaseModel"
+        ):
+            return error_messages[command][1]  # class doesn't exist in create
+
+        if command == "update":
+            if len(command_args) < 3:
+                return error_messages[command][3]  # attribute name missing
+            if len(command_args) < 4:
+                return error_messages[command][4]  # value missing
+
         return False
-    
+
     def read_basemodel_file(self):
         """
         Opens and reads file.json that contains all instances of BaseModel
@@ -63,7 +79,7 @@ class HBNBCommand(cmd.Cmd):
         except (FileNotFoundError, json.JSONDecodeError) as e:
             print(f"Error reading file: {e}")
         return all_basemodel_instance
-    
+
     def write_to_basemodel_file(self, basemodel_instances):
         """
         Opens and writes to file.json
@@ -84,7 +100,7 @@ class HBNBCommand(cmd.Cmd):
 
         Args:
             object_id (str): The ID to search for.
-            basemodel_instances (dict, optional): A dictionary of instances, 
+            basemodel_instances (dict, optional): A dictionary of instances,
                 where each value is a dictionary containing an "id" key. Defaults to None.
 
         Returns:
@@ -101,7 +117,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             raise TypeError("Expected a dictionary for 'basemodel_instances'.")
         return None
-    
+
     def do_create(self, line):
         """
         Creates a new instance of BaseModel.
@@ -125,7 +141,7 @@ class HBNBCommand(cmd.Cmd):
         """
         Displays the string representation
         of a class instance based on the id
-        
+
         Usage:
             show Basemodel (id) # replace (id) with the actual id
 
@@ -137,7 +153,9 @@ class HBNBCommand(cmd.Cmd):
         else:
             instance_id = line.split()[1]
             all_instances = self.read_basemodel_file()
-            instance_info = self.id_exists(instance_id, basemodel_instances=all_instances)
+            instance_info = self.id_exists(
+                instance_id, basemodel_instances=all_instances
+            )
             if instance_info:
                 instance_dict = instance_info[1]
                 base_model_instance = BaseModel(**instance_dict)
@@ -148,7 +166,7 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, line):
         """
         Deletes an instance from the file using its id
-        
+
         Usage:
             destroy BaseModel (id) # replace (id) with the actual id
         """
@@ -158,7 +176,9 @@ class HBNBCommand(cmd.Cmd):
         else:
             instance_id = line.split()[1]
             all_instances = self.read_basemodel_file()
-            instance_info = self.id_exists(instance_id, basemodel_instances=all_instances)
+            instance_info = self.id_exists(
+                instance_id, basemodel_instances=all_instances
+            )
             if instance_info:
                 key = instance_info[0]
                 del all_instances[key]
@@ -167,9 +187,7 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_all(self, line):
-        """
-        
-        """
+        """ """
         if not line or line == "BaseModel":
             all_instances = self.read_basemodel_file()
             for key, instance_dict in all_instances.items():
@@ -178,51 +196,26 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    def do_update(self, line):
+        """ """
+        error_message = self.validate_command(line, "update")
+        if error_message:
+            print(error_message)
+        else:
+            instance_id = line.split()[1]
+            new_attribute, new_attribute_value = line.split()[2:4]
+            all_instances = self.read_basemodel_file()
+            instance_info = self.id_exists(
+                instance_id, basemodel_instances=all_instances
+                )
+            if instance_info:
+                key, instance_dict = instance_info
+                instance_dict[new_attribute] = new_attribute_value # typecast to the type or class
+                all_instances[key] = instance_dict
+                self.write_to_basemodel_file(all_instances)
+            else:
+               print("** no instance found **") 
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def do_show(self, line):
-#         """
-#         Displays the string representation
-#         of a class instance based on the id
-        
-#         Usage:
-#             show Basemodel (id) # replace (id) with the actual id
-
-#         """
-#         error = self.err_messages(line, "show")
-
-#         if not error:
-#             args = line.split()
-#             file_path = os.path.abspath("file.json")
-#             id_found = False
-#             with open(file_path, "r") as basemodel_file:
-#                 base_model_obj = json.load(basemodel_file)
-#             for key, obj in base_model_obj.items():
-#                 if args[1] == obj["id"]:
-#                     id_found = True
-#                     base_model_instance = BaseModel(**obj)
-#                     print(base_model_instance)
-#                     break
-#             if id_found == False:
-#                 print("** no instance found **")
-#         else:
-#             print(error)
