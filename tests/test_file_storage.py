4fe8bb158a28c402e179c509d4cb46780272f091
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-11-17 10:53:04 +0100

    delete tearDown method, modified setUp method

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -5,7 +5,6 @@
 """
 
 import unittest
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
@@ -16,12 +15,6 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """ """
         self.my_model = BaseModel()
-        self.storage = FileStorage()
-        self.storage.reload()
-
-    def tearDown(self):
-        """ """
-        FileStorage._FileStorage__objects = {}
 
     def test_save_first_base_model_instance(self):
         """ """
