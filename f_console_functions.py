df1a2886e37271ac21799b300b57688bf276f491
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-11-23 07:26:29 +0100

    add extract_valid_args() function

--- f_console_functions.py
+++ f_console_functions.py
@@ -5,11 +5,12 @@
 """
 import json
 import os
+import re
 import keyword
 from models.base_model import BaseModel
 
 
-def validate_command_args(line, command):
+def validate_command_args(line, cmd):
     """
     Validates the arguments of console commands.
 
@@ -21,7 +22,7 @@ def validate_command_args(line, command):
         str: An error message if validation fails, otherwise False.
     """
 
-    command_args = line.split()
+    cmd_args = line.split()
     classes = [
         "BaseModel", "User", "State", "City",
         "Amenity", "Place", "Review"
@@ -31,36 +32,37 @@ def validate_command_args(line, command):
     msg2 = "** instance id missing **"
     msg3 = "** attribute name missing **"
     msg4 = "** value missing **"
-    error_messages = {
+    err_msg = {
         "create": [msg0, msg1],
+        "count": [msg0, msg1],
         "show": [msg0, msg1, msg2],
         "destroy": [msg0, msg1, msg2],
         "update": [msg0, msg1, msg2, msg3, msg4],
     }
-    if not command_args:
-        return error_messages[command][0]  # class name missing
+    if not cmd_args:
+        return err_msg[cmd][0]  # class name missing
 
-    if command_args[0] not in classes and command != "create":
-        return error_messages[command][1]  # class doesn't exist
+    if cmd_args[0] not in classes and cmd not in ["create", "count"]:
+        return err_msg[cmd][1]  # class doesn't exist
 
-    if len(command_args) < 2 and command in ["show", "destroy", "update"]:
-        return error_messages[command][2]  # instance id missing
+    if len(cmd_args) < 2 and cmd in ["show", "destroy", "update"]:
+        return err_msg[cmd][2]  # instance id missing
 
-    if command == "create" and (
-        len(command_args) > 1 or command_args[0] not in classes
+    if cmd in ["create", "count"] and (
+        len(cmd_args) > 1 or cmd_args[0] not in classes
     ):
-        return error_messages[command][1]  # class doesn't exist in create
+        return err_msg[cmd][1] # class doesn't exist in 'create' or 'count'
 
-    if command == "update":
-        if len(command_args) < 3:
-            return error_messages[command][3]  # attribute name missing
-        if len(command_args) < 4:
-            return error_messages[command][4]  # value missing
+    if cmd == "update":
+        if len(cmd_args) < 3:
+            return err_msg[cmd][3]  # attribute name missing
+        if len(cmd_args) < 4:
+            return err_msg[cmd][4]  # value missing
 
     return False
 
 
-def id_exists(object_id, class_name, all_instances):
+def id_exists(obj_id, cls_name, all_instances):
     """
     Check if the provided object_id exists in all_instances.
 
@@ -74,11 +76,11 @@ def id_exists(object_id, class_name, all_instances):
         None: If no match is found or an error occurs.
     """
     try:
-        for key, instance_dict in all_instances.items():
-            instance_dict = instance_dict.to_dict()
-            if (object_id == instance_dict["id"] and
-                class_name == instance_dict["__class__"]):
-                return (key, instance_dict)
+        for key, inst_addr in all_instances.items():
+            inst_dict = inst_addr.to_dict()
+            if (obj_id == inst_dict["id"] and
+                cls_name == inst_dict["__class__"]):
+                return (key, inst_addr)
     except Exception as e:
         print(e)
     return None
@@ -136,3 +138,33 @@ def validate_attribute(attribute_name):
         return False
 
     return True
+
+
+def extract_valid_args(line):
+    """
+    
+    """
+    pattern1 = r'"([^"]+)"|([^,\s]+)'
+    dict_pattern = r'([^"]+)\s"([^"]+)",?\s*(\{.*?\})'
+    
+    if "{" not in line:
+        first_match = re.findall(pattern1, line)
+        if first_match:
+            found_match = [match[0] or match[1] for match in first_match]
+            return " ".join(found_match)
+    else:
+        second_match = re.search(dict_pattern, line)
+        if second_match:
+            return " ".join(second_match.groups())
+    
+    return line
+        
+
+if __name__ == "__main__":
+    line1 =  'Place "39eaf3b1-f641-4fd0-94a0-d071b7b868ed", "age", 54, firsname, Betty'
+    line2 = """BaseModel "38f22813-2753-4d42-b37c-57a17f1e4f88", {'first_name': "John", "age": 89}"""
+    line3 = "User 39eaf3b1-f641-4fd0-94a0-d071b7b868ed age 54"
+    line4 = 'User "95c0f625-a76f-4c75-9fe2-a2a7531d8b2d" {"first_name": Kelly, "age": 26}'
+
+    result = extract_valid_args(line2)
+    print(result)
